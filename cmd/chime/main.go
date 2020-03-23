ef46d0992dc443472b6b0274098186ad04e90cf3
Stuart Leeks <stuart@leeks.net>
2020-03-23 21:12:13 +0000

    Add initial retry logic to chime

--- cmd/chime/main.go
+++ cmd/chime/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/url"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/stuartleeks/pi-bell/internal/pkg/events"
@@ -20,16 +23,32 @@ const ChipName string = "gpiochip0"
 
 func main() {
 	flag.Parse()
+	address := addr
 
+	for {
+		err := connectAndHandleEvents(address)
+
+		if err == nil {
+			// handler returned so was interrupted by user
+			log.Println("Exiting")
+			break
+		}
+		log.Printf("Failed to connect: (%T) %v\n", err, err)
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func connectAndHandleEvents(address *string) error {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/doorbell"}
+	u := url.URL{Scheme: "ws", Host: *address, Path: "/doorbell"}
 	log.Printf("connecting to %s", u.String())
 
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
 	if err != nil {
-		log.Fatal("dial:", err) // TODO - need to think about resiliency
+		err = fmt.Errorf("dial to %s failed: %v", u.String(), err)
+		return err
 	}
 	defer c.Close()
 
@@ -44,19 +63,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer relay.Close()
 
+	resultChan := make(chan error, 1)
 	log.Printf("Listening...\n")
 	go func() {
 		for {
 			messageType, buf, err := c.ReadMessage()
 			if err != nil {
-				log.Printf("Error reading: %v\n", err)
+				log.Printf("Error reading:  (%T) %v\n", err, err) // TODO - check for websocket.CloseError and return to trigger reconnecting? (Currently panics for repeated read on failed connection in websocket code)
+				// resultChan <- err
+				// return
+				var closeError *websocket.CloseError
+				var opErr *net.OpError
+				if errors.As(err, &closeError) ||
+					errors.As(err, &opErr) {
+					resultChan <- err
+					return
+				}
 				continue
 			}
 			log.Printf("Received: %v: %s\n", messageType, string(buf))
 			buttonEvent, err := events.ParseButtonEventJSON(buf)
 			if err != nil {
-				log.Printf("Error parsing: %v\n", err)
+				log.Printf("Error parsing: (%T) %v\n", err, err)
 				continue
 			}
 
@@ -68,12 +98,17 @@ func main() {
 				log.Println("Turning relay off")
 				relay.Off()
 			default:
-				log.Printf("Unhandled ButtonEventType: \n", buttonEvent.Type)
+				log.Printf("Unhandled ButtonEventType: %v \n", buttonEvent.Type)
 			}
 		}
 	}()
 
-	// TODO - also exit when goroutine finishes
-
-	<-interrupt
+	select {
+	case <-interrupt:
+		log.Println("Returning from connectAndHandleEvents - no error")
+		return nil
+	case err := <-resultChan:
+		log.Printf("Returning from connectAndHandleEvents - error (%T): %s\n", err, err)
+		return err
+	}
 }
